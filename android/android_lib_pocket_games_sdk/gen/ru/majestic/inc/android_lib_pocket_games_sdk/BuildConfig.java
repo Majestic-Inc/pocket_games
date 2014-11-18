4713da368fe8791dc6b31277c4696634064f6564
MRMajesticMR <mrmajesticmr90@gmail.com>
2014-11-19 01:37:09 +0400

    Создан проект-шаблон для библиотеки.

---
+++ android/android_lib_pocket_games_sdk/gen/ru/majestic/inc/android_lib_pocket_games_sdk/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ru.majestic.inc.android_lib_pocket_games_sdk;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
