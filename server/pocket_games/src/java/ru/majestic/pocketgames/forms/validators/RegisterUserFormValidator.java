62ebfe649aedf3992a20d83c1b36d77cb136326c
MRMajesticMR <mrmajesticmr90@gmail.com>
2014-11-19 20:39:17 +0300

    Сделана проверка длины имени Сделана проверка длины фамилии

--- server/pocket_games/src/java/ru/majestic/pocketgames/forms/validators/RegisterUserFormValidator.java
+++ server/pocket_games/src/java/ru/majestic/pocketgames/forms/validators/RegisterUserFormValidator.java
@@ -20,12 +20,18 @@ public class RegisterUserFormValidator {
     
     private static final int ERROR_CODE_PASSWORDS_NOT_EQUALS        = 104;
     
+    private static final int ERROR_CODE_NAME_LONG                   = 105;
+    private static final int ERROR_CODE_LASTNAME_LONG               = 106;
     
-    private static final int E_MAIL_MIN_LENGTH  = 3;
-    private static final int E_MAIL_MAX_LENGTH  = 128;    
     
-    private static final int PASSWORD_MIN_LENGTH  = 5;    
-    private static final int PASSWORD_MAX_LENGTH  = 64;    
+    private static final int E_MAIL_MIN_LENGTH      = 3;
+    private static final int E_MAIL_MAX_LENGTH      = 128;    
+    
+    private static final int PASSWORD_MIN_LENGTH    = 5;    
+    private static final int PASSWORD_MAX_LENGTH    = 64;
+    
+    private static final int NAME_MAX_LENGTH        = 64;
+    private static final int LASTNAME_MAX_LENGTH    = 64;
     
     public static ValidationStatus validateUserRegistrationFormObject(UserRegistrationFormObject userRegistrationFormObject) {
         if(userRegistrationFormObject.getMail().length() < E_MAIL_MIN_LENGTH) {
@@ -46,6 +52,13 @@ public class RegisterUserFormValidator {
         
         if(!userRegistrationFormObject.getPassword().equals(userRegistrationFormObject.getRepeatPassword())) {
             return new ValidationStatus(ValidationStatus.Status.ERROR, ERROR_CODE_PASSWORDS_NOT_EQUALS, "Пароли не совпадают.");
+        }
+        
+        if(userRegistrationFormObject.getName().length() > NAME_MAX_LENGTH) {
+            return new ValidationStatus(ValidationStatus.Status.ERROR, ERROR_CODE_NAME_LONG, "Имя слишком длинное. Максимальная длина " + NAME_MAX_LENGTH);
+        }
+        if(userRegistrationFormObject.getLastName().length() > LASTNAME_MAX_LENGTH) {
+            return new ValidationStatus(ValidationStatus.Status.ERROR, ERROR_CODE_LASTNAME_LONG, "Фамилия слишком длинная. Максимальная длина " + LASTNAME_MAX_LENGTH);
         }
         
         return new ValidationStatus(ValidationStatus.Status.OK);
